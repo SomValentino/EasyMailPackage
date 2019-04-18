f1b0c40a1e32691243ce13c8633c167ad92c2d61
valentine azom <valentine.azom@entelect.co.za>
2019-04-18 07:50:46 +0200

    Revert "First push of Common.Email.Package including:"
    This reverts commit 2278cc4f2c62e92b69eb3446bf8756e4444bb83f.

--- Common.Email.Package/Enums/Status.cs
+++
@@ -1,8 +0,0 @@
-namespace Common.Email.Package.Enums
-{
-    public enum Status
-    {
-        Success = 1,
-        Failure
-    }
-}
--- Common.Email.Package/MessageInformation/Interfaces/IEmailAddress.cs
+++
@@ -1,8 +0,0 @@
-namespace Common.Email.Package.MessageInformation.Interfaces
-{
-    public interface IEmailAddress
-    {
-         string Name {get; set;}
-         string Address {get; set;}
-    }
-}
